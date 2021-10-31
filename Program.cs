2593439c90045683f3530e58f3ae3e40ba99c3ed
Виктор Осипов <v.osipov@hostco.ru>
2021-10-31 15:21:45 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsharpStudyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
