0fe2dbed1f069ca32419726ff97afde9f94b78b3
Виктор Осипов <v.osipov@hostco.ru>
2021-11-06 01:56:35 +0500

    Болванки упражнений

---
+++ Milestones/Module1/TypeBasics/MyImmutableType.cs
@@ -0,0 +1,10 @@
+﻿namespace CsharpStudyApp.Milestones.Module1.TypeBasics
+{
+    public class MyImmutableType
+    {
+        /*
+         * Создайте свой иммутабельный (immutable) тип. При изменении какого-либо из полей
+         * создается новый объект. В качестве примера можете взять тип String.
+         */
+    }
+}
---
+++ Milestones/Module1/TypeBasics/VirtualMethodImitation.cs
@@ -0,0 +1,10 @@
+﻿namespace CsharpStudyApp.Milestones.Module1.TypeBasics
+{
+    public class VirtualMethodImitation
+    {
+        /*
+         * Попробуйте симитировать наследование и переопределение виртуального метода в структурах
+         */
+        
+    }
+}
